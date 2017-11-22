9760b8c29a86ec19c8939be63a5f6b84e8f45b98
JesusRuiver <jesusruiver@jesusruiver.com>
2017-11-22 21:32:11 +0100

    Commit

---
+++ app/src/main/java/asteroides/examples/org/asteroides/Grafico.java
@@ -0,0 +1,8 @@
+package asteroides.examples.org.asteroides;
+
+/**
+ * Created by Vespertino on 22/11/2017.
+ */
+
+public class Grafico {
+}
---
+++ app/src/main/java/asteroides/examples/org/asteroides/Juego.java
@@ -0,0 +1,15 @@
+package asteroides.examples.org.asteroides;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Vespertino on 22/11/2017.
+ */
+
+public class Juego extends Activity {
+    @Override public void onCreate (Bundle savedInsState){
+        super.onCreate(savedInsStateState);
+        setContentView(R.layout.juego);
+    }
+}
